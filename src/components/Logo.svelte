8f7b61fffdf71fead41721e60a50f786490e5e42
stordahl <jacob@blkcatstudio.com>
2020-10-05 16:42:53 -0500

    Logo issues mobile fix

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -4,9 +4,6 @@
     h1{
         font-size:4.5rem;
         margin:1rem 2rem;
-        position: absolute;
-        top:0;
-        z-index:2;
         font-weight:bold;
        
     }
@@ -17,6 +14,9 @@
         h1{
             font-size:6rem;
             margin:0 2rem;
+            position: absolute;
+            top:0;
+            z-index:2;
         }
     }
 </style>
